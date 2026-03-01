50c4abc4ae59d2705a390c1c9fd0c0305a7c2444
gogcat <gogcat.lee@gmail.com>
2026-03-01 08:15:42 +0800

    ignore .open-next build directory

--- .open-next/assets/_next/static/chunks/app/(admin)/layout-c9bec412c344da7d.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[4012],{872:(e,s,_)=>{Promise.resolve().then(_.t.bind(_,7235,23))}},e=>{e.O(0,[7235,4523,3657,7358],()=>e(e.s=872)),_N_E=e.O()}]);
